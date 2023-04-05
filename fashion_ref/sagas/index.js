837643cbf83fe31768dbbe60b9b9c84ff2ab8f3c
izr8809 <izr8809@gmail.com>
2023-04-05 12:41:29 +0900

    @

--- fashion_ref/sagas/index.js
+++ fashion_ref/sagas/index.js
@@ -3,8 +3,8 @@ import postSaga from "./post";
 import userSaga from "./user";
 import axios from "axios";
 
-// axios.defaults.baseURL = 'http://15.164.224.172:8080/';
-axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.baseURL = 'http://15.164.224.172:8080/';
+// axios.defaults.baseURL = 'http://localhost:8080/';
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
