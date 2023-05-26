210c0d41200e5d22c9db15867b7b69052cb6a395
Seonho Cho <jshone144@gmail.com>
2023-05-27 08:09:30 +0900

    add port

--- fashion_ref/ecosystem.config.js
+++ fashion_ref/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "FashionRef",
     script : "node_modules/next/dist/bin/next",
-    args: "start",
+    args: "start -p 8080",
     env_production: {
       APP_ENV: 'prod'
     }
