366d8db4e263b1e3aa6feb154d8c45cea3aea8b6
izr8809 <izr8809@gmail.com>
2023-05-28 19:13:48 +0900

    todeploy

--- fashion_ref/Components/MainView.tsx
+++ fashion_ref/Components/MainView.tsx
@@ -77,7 +77,7 @@ export default function MainView({}: MainViewProps) {
             id : lastWorkspaceId
           }
         });
-        if(lastReferenceId || lastBoardId){
+        if(lastReferenceId || lastBoardId){ 
           dispatch({
             type: CHANGE_CURRENTSPACE_ID,
             data: {
